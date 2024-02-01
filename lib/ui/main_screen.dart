4b110359784bdb4ac50c8a944f8d8b30a3e18ae0
Eye Level <138679059+I-AQ@users.noreply.github.com>
2024-02-01 19:14:35 +0100

    Update main_screen.dart

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,11 +1,12 @@
 import 'package:app_client/blocs/notes_state.dart';
 import 'package:app_client/constants/custom_colors.dart';
 import 'package:app_client/ui/appbar/main_app_bar.dart';
-import 'package:app_client/ui/main_screen_with_content.dart';
+import 'package:app_client/ui/main_screen_with_content_grid.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../blocs/notes_cubit.dart';
 import 'main_screen_empty.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -26,7 +27,7 @@ class MainScreen extends StatelessWidget {
           if (snapshot.data!.currentNotes.isEmpty) {
             return const MainScreenEmpty();
           } else {
-            return MainScreenWithContent(
+            return MainScreenWithContentGridView(
               notes: snapshot.requireData.currentNotes.reversed.toList(),
             );
           }
